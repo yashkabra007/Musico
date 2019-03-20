514f80e44542df0b7f9fede9f422168301a20693
yashkabra007 <yashkabra007.yk@gmail.com>
2019-03-20 05:33:24 +0530

    made a user profile model

--- music/models.py
+++ music/models.py
@@ -1,5 +1,7 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 class Album(models.Model):
     artist = models.CharField(max_length=250)
@@ -19,3 +21,22 @@ class Song(models.Model):
 
     def __str__(self):
         return self.song_title
+
+
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    favourite_songs = models.ManyToManyField(Song, related_name='profiles')
+    profile_photo = models.FileField()
+    bio = models.CharField(max_length=20)
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+
